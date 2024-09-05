08d7236b2ebe8426b5f1feeb46d69d4012bfc2ba
Aman Joshi <amanjoshi2527@gmail.com>
2024-09-05 11:34:24 +0530

    cahnge in logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,9 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logoCodeDploy.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-// import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
@@ -18,21 +17,25 @@ export const NavBar = () => {
       } else {
         setScrolled(false);
       }
-    }
+    };
 
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="/">
-          <img src={logo} alt="Logo" style={{ backgroundColor: 'transparent' }} />
+          <img
+            src={logo}
+            alt="Logo"
+            className={scrolled ? "scrolled-logo" : "default-logo"} // Add class dynamically
+          />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
@@ -41,18 +44,13 @@ export const NavBar = () => {
           <Nav className="ms-auto">
             <Nav.Link href="/#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="/#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Services</Nav.Link>
-            {/* Use Link for About to navigate to a new page */}
             <Link to="/about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About</Link>
             <Nav.Link href="/#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Our Work</Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/company/codedeploy/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="" /></a>
-              {/* <a href="#"><img src={navIcon2} alt="" /></a> */}
-              <a href="https://www.instagram.com/codedeploy_?igsh=NHcxejk2eDM0YW02" target="_blank" rel="noopener noreferrer">
-  <img src={navIcon3} alt="Instagram" />
-</a>
-
+              <a href="https://www.linkedin.com/company/codedeploy/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
+              <a href="https://www.instagram.com/codedeploy_?igsh=NHcxejk2eDM0YW02" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="Instagram" /></a>
             </div>
             <HashLink to='/#connect'>
               <button className="vvd"><span>Start a Project</span></button>
@@ -61,5 +59,5 @@ export const NavBar = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
